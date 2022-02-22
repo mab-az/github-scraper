4aee042f6b402c935c9b1f720f9dde4ce25b4e1e
mab-az <malgorzata.balcerzak@astrazeneca.com>
2022-02-22 16:37:34 +0100

    contributors ready scripts

--- src/user.py
+++ src/user.py
@@ -1,14 +1,13 @@
 import json
 import requests
+import os
 
 
 def extract_data(DataNeeded, DataFromGithub, ):
     Data = {}
     for (k, v) in DataFromGithub.items():
-
             if k in DataNeeded:
                 Data[k] = v
-    
     return Data
 
 
@@ -35,52 +34,65 @@ class User():
         return self.UserData
 
 
-def users_left_to_scape():
-    with open('results/az_contributors.json', 'r') as f:
-        users_p = json.load(f)
-
-    for user in users_p:
-        username = user['login']
-        with open(f'user_profiles/{username}.json', 'w') as f:
-            json.dump(user, f)
-
-    users_present = [x['login'] for x in list(users_p)]
+def scrape_all_users():
+    """ 
+    Takes a list of Organisation members and scrapers each individual's profile 
+    BLOCKED: API is blocked after 60 requests in one hour
+    """
+    contributors = []
 
     with open('results/az_users_list.json', 'r') as f:
-        users_all = json.load(f)
+        users = json.load(f)
 
-    diff = [x for x in users_all if x not in users_present]
+    for username in users:
+        print(f"Username: {username}")
+        user_stats = User(username).get_user_stats()
+        contributors.append({"author": user_stats})
 
-    print(diff)
-    print(len(diff), len(users_all))
+    with open('results/contributors.json', 'w') as f:
+        json.dump(contributors, f)
+
+
+def create_individual_profiles():
+    with open('results/az_users_list.json', 'r') as f:
+        users_to_add = json.load(f)
+
+    print(f"All contrinutors: {len(users_to_add)}")
+    diff = []
+
+    for username in users_to_add:
+        user_stats = User(username).get_user_stats()
+        if user_stats:
+            print(f"Adding: {username}")
+            with open(f'user_profiles/{username}.json', 'w') as f:
+                json.dump(user_stats, f)
+        else:
+            diff.append(username)
+
+    print(f"Newly scraped: {len(users_to_add) - len(diff)}")
+    print(f"Missing: {len(diff)}")
 
     with open(f'results/users_to_add.json', 'w') as f:
             json.dump(diff, f)
 
 
+def combine_individual_profies():
+    """Creates the contrinutors.json file to be used on the website"""
 
+    contributors = []
+
+    for user in os.listdir('user_profiles'):
+        with open(f"user_profiles/{user}", 'r') as f:
+            profile = json.load(f)
+
+        contributors.append({"author": profile})
+
+    with open('results/contributors.json', 'w') as f:
+        json.dump(contributors, f) 
 
 
 
 if __name__ == "__main__":
-    # """ Takes a list of Organisation members and scrapers each individual's profile """
-    # contributors = []
-
-    # with open('results/az_users_list.json', 'r') as f:
-    #     users = json.load(f)
-
-    # for username in users:
-    #     print(f"Username: {username}")
-    #     user_stats = User(username).get_user_stats()
-
-    #     # print(f"User stats: {len(user_stats)}")
-
-    #     contributors.append({"author": user_stats})
-
-    # with open('results/contributors.json', 'w') as f:
-    #     json.dump(contributors, f)
-
-
-    
-    users_left_to_scape()
+    # create_individual_profiles()
+    combine_individual_profies()
     
