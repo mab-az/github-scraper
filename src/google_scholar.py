55616a82ca2f4e0e8ed1fd62e30884fe59a974da
mab-az <malgorzata.balcerzak@astrazeneca.com>
2022-05-09 15:06:16 +0200

    sorted paper list by year

--- src/google_scholar.py
+++ src/google_scholar.py
@@ -2,6 +2,7 @@ import json
 import requests
 import os
 from bs4 import BeautifulSoup as bs
+import re
 
 
 to_remove = ['<div class="gs_gray">', '<span class="gs_oph">', '</span></div>', '</div>', '\n']
@@ -36,20 +37,27 @@ def scholar_parser(file_path):
                     authors = link2
                 else:
                     publisher = link2
+                    year_published = re.findall(r'20[0-9]{2}', str(publisher))
+                    article_year = year_published[0] 
 
             papers.append({
                 'authors': authors, 
                 'title': title, 
                 'paper_link': f"https://scholar.google.com{paper_link}", 
-                'publisher': publisher
+                'publisher': publisher,
+                'year': article_year
                 })
 
+    sorted_papers = sorted(papers, key=lambda x: x['year'], reverse=True)
 
     with open('google_scholar_ehsan.json', 'w', encoding='utf-8') as f:
-        json.dump(papers, f)
+        json.dump(sorted_papers, f)
                 
 
 def format_json_scholar(path):
+    # pattern - e.g. 2020, 2013 ... 
+    # pattern = r'20[0-9][{2}'
+
     with open(path, 'r') as f:
         papers = json.load(f)
 
@@ -58,13 +66,12 @@ def format_json_scholar(path):
             title = paper['title']
             paper_link = paper['paper_link']
             publisher = paper['publisher']
+            year = paper['year']
 
-            print(f" - {author} [{title}]({paper_link}), {publisher}")
-
-
+            print(f" - **[{title}]({paper_link})**, {year}")
 
 
 if __name__ == "__main__":
     # scholar_parser('scholar_html/scholar_ehsan.html')
 
-    format_json_scholar('google_scholar_ehsan.json')
+    format_json_scholar('year_ordered_papers.json')
